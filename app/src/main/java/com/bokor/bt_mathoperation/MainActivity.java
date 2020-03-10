9a4a52ced5268c22da90391151a1653e2a28de65
Thean19 <Thean19@users.noreply.github.com>
2020-03-10 12:06:32 +0700

    some animation for text and background

--- app/src/main/java/com/bokor/bt_mathoperation/MainActivity.java
+++ app/src/main/java/com/bokor/bt_mathoperation/MainActivity.java
@@ -2,19 +2,38 @@ package com.bokor.bt_mathoperation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_click,btn_click2,btn_click3,btn_click4,btn_click5;
+    Button btn_gone;
+    Animation fadeout,fadein;
+    TextView show_text;
+    ViewGroup container;
+    boolean visible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        show_text=findViewById(R.id.show_animation_text);
+        fadeout= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+        fadein= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        container=findViewById(R.id.transition_container);
+
+        btn_gone = findViewById(R.id.btn_gone);
 
         btn_click = findViewById(R.id.sound_click);
         btn_click2 = findViewById(R.id.sound_click2);
@@ -31,31 +50,167 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mp.start();
+                TransitionManager.beginDelayedTransition(container);
+                visible = !visible;
+                show_text.setVisibility(visible ? View.VISIBLE: View.VISIBLE);
+                show_text.setText("Click to play sound 1!");
             }
         });
         btn_click2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mp2.start();
+                TransitionManager.beginDelayedTransition(container);
+                visible = !visible;
+                show_text.setVisibility(visible ? View.VISIBLE: View.VISIBLE);
+                show_text.setText("Click to play sound 2!");
             }
         });
         btn_click3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mp3.start();
+                TransitionManager.beginDelayedTransition(container);
+                visible = !visible;
+                show_text.setVisibility(visible ? View.VISIBLE: View.VISIBLE);
+                show_text.setText("Click to play sound 3!");
             }
         });
         btn_click4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mp4.start();
+                TransitionManager.beginDelayedTransition(container);
+                visible = !visible;
+                show_text.setVisibility(visible ? View.VISIBLE: View.VISIBLE);
+                show_text.setText("Click to play sound 4!");
             }
         });
         btn_click5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mp5.start();
+                TransitionManager.beginDelayedTransition(container);
+                visible = !visible;
+                show_text.setVisibility(visible ? View.VISIBLE: View.VISIBLE);
+                show_text.setText("Click to play sound 5!");
             }
         });
+
+        btn_gone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TransitionManager.beginDelayedTransition(container);
+                visible = !visible;
+                show_text.setVisibility(visible ? View.GONE: View.GONE);
+            }
+        });
+        for_animation_button();
+    }
+
+    private void for_animation_button(){
+        btn_click.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        view.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                        view.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        view.getBackground().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+        btn_click2.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        view.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                        view.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        view.getBackground().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+        btn_click3.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        view.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                        view.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        view.getBackground().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+        btn_click4.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        view.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                        view.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        view.getBackground().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+
+        btn_click5.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        view.getBackground().setColorFilter(0xe0f47521, PorterDuff.Mode.SRC_ATOP);
+                        view.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP: {
+                        view.getBackground().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    private void animation_for_text(){
+
     }
 }
---
+++ app/src/main/java/com/bokor/bt_mathoperation/Splashscreen.java
@@ -0,0 +1,34 @@
+package com.bokor.bt_mathoperation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashTimeOut=5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        logo=(ImageView)findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splashscreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
+        logo.startAnimation(myanim);
+    }
+}
