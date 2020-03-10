b08369aa25b409551a252a16d4aa28e9accdb7bf
Thean19 <Thean19@users.noreply.github.com>
2020-03-10 09:04:26 +0700

    sound click

--- app/src/main/java/com/bokor/bt_mathoperation/MainActivity.java
+++ app/src/main/java/com/bokor/bt_mathoperation/MainActivity.java
@@ -2,13 +2,28 @@ package com.bokor.bt_mathoperation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_click = findViewById(R.id.sound_click);
+        final MediaPlayer mp=MediaPlayer.create(this, R.raw.bells003);
+
+        btn_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+            }
+        });
     }
 }
